dd9bb85d2cc014f803da30907ea5593b846b4958
asb1122 <asb1122@yandex.ru>
2017-09-19 22:38:52 +0300

    Product Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const bigInt = require('./bigInt.js');
+
 module.exports = function multiply(first, second) {
-  // your solution
+  var num1 = bigInt(first);
+  var num2 = bigInt(second);
+  var multiply = num1.multiply(num2);
+  return multiply.toString();
 }
